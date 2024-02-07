a3388aa831f5db245543d6c3d23d47957b6e2b54
Douglas Melo <douglasmnegri@gmail.com>
2024-02-06 21:24:56 -0300

    add a condition before enabling button

--- file.js
+++ file.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("myForm");
   const password = document.getElementById("password");
   const pwd = document.getElementById("pwd");
+  const errorMessage = document.querySelector(".error")
 
   const updatePasswordMatch = () => {
     const passwordValue = password.value;
@@ -10,15 +11,41 @@ document.addEventListener("DOMContentLoaded", () => {
     if (passwordValue === pwdValue) {
       password.style.backgroundColor = "lightgreen";
       pwd.style.backgroundColor = "lightgreen";
+      errorMessage.textContent = ""
     } else {
       password.style.backgroundColor = "tomato";
       pwd.style.backgroundColor = "tomato";
+      errorMessage.textContent = "*Passwords do not match";
     }
   };
 
   pwd.addEventListener("input", updatePasswordMatch);
+  password.addEventListener("input", updatePasswordMatch);
   form.addEventListener("submit", function (event) {
-    event.preventDefault();
     updatePasswordMatch();
+  })
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.querySelector("button");
+  const firstName = document.querySelector("#name");
+  const lastName = document.querySelector("#last-name");
+  button.disabled = true;
+
+  firstName.addEventListener("input", () => {
+    if(firstName.value !== "" && lastName.value !== "") {
+      button.disabled = false;
+    }
+    else {
+      button.disabled = true;
+    }
+  });
+  lastName.addEventListener("input", () => {
+    if(firstName.value !== "" && lastName.value !== "") {
+      button.disabled = false;
+    }
+    else {
+      button.disabled = true;
+    }
   })
 });
